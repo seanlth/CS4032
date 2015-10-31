fef05c139a04f35b1da7e79df14b1173b85db873
hargados <hargados@tcd.ie>
2015-10-31 18:59:58 +0000

    fixed compile.sh

--- Lab2/compile.sh
+++ Lab2/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clang -c src/get_host.c
-ar rcs libhost.a get_host.o
+ar rcs /src/libhost.a src/get_host.o
 cp src/libhost.a target/debug/deps/
 rm src/libhost.a src/get_host.o
 cargo build
