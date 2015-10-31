8498d9626f9c015033f2cf1520b8c404ec926a3f
hargados <hargados@tcd.ie>
2015-10-31 19:04:22 +0000

    fixed paths

--- Lab2/compile.sh
+++ Lab2/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clang -c src/get_host.c
-ar rcs libhost.a src/get_host.o
+ar rcs libhost.a get_host.o
 cp libhost.a target/debug/deps/
-rm libhost.a src/get_host.o
+rm libhost.a get_host.o
 cargo build
